3aa35b459e3e6b24b47d71e4968e033873ebb2ba
Blake Sweeney <blakes.85@gmail.com>
2014-10-01 14:37:24 -0400

    Change modnum, range1 and range2 types
    These should all be numbers, not strings. They are now converted to numbers.

--- db.py
+++ db.py
@@ -69,10 +69,10 @@ def connection_test():
 
         #   Manually binding the default values for initial test.
         pdbid   = "2AW7"
-        modnum  = "1"
+        modnum  = 1
         chainid = "A"
-        range1  = "887"
-        range2  = "894"
+        range1  = 887
+        range2  = 894
 
         seqvar_range_1(conn, pdbid, modnum, chainid, range1, range2)
         results_svr1(conn)
@@ -123,7 +123,7 @@ def seqvar_range_1(conn, pdbid, modnum, chainid, range1, range2):
     """
     proc = conn.init_procedure('SeqVar_Range1')
 
-    proc.bind(pdbid, _mssql.SQLCHAR, 'PDBID', 'False', 'False', 4)
+    proc.bind(pdbid, 'SQLCHAR', 'PDBID', 'False', 'False', 4)
     proc.bind(modnum, 'SQLINT1', 'ModNum', 'False', 'False')
     proc.bind(chainid, 'SQLCHAR', 'ChainID', 'False', 'False', 1)
     proc.bind(range1, 'SQLCHAR', 'range1', 'False', 'False')
