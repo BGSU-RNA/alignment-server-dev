1a6260bb513bf7192f4e1bd066232c976feeb0ec
Blake Sweeney <blakes.85@gmail.com>
2014-10-07 13:25:36 -0400

    Add templates and webcode to render them
    We now have multiple possible output content types, application/json and
    text/html. This is useful for both input and output. In the case of input we
    will provide a simple page with a useful form to fill out. For output we can
    produce a simple HTML page to display the data. Also, by allowing a JSON output
    we can integrate this tool with other web sites and services.
    
    Right now all templates are just stubs but they will be filled out later as we
    settle on a design.

--- app.py
+++ app.py
@@ -1,39 +1,61 @@
+import simplejson as json
+
 from flask import g
 from flask import Flask
 from flask import request
-from flask.views import MethodView
+from flask import render_template
 
+import db
 import utils as ut
 
+from flask_mime import Mime
+
 app = Flask(__name__)
-
-# @app.before_request
-# def before_request():
-#     pass
-#     # g.seq_db = ut.connect_seq()
-
-# @app.teardown_request
-# def teardown_request(exception):
-#     db = getattr(g, 'seq_db', None)
-#     if db is not None:
-#         db.close()
-
-@app.route('/variation', methods=['GET'])
-def get():
-    units = request.args['units']
-    sequences = request.args.get('sequences', None)
-    return ut.compute_variation(units, sequences)
+mimetype = Mime(app)
 
 
-# @app.route('/variation', methods=['POST'])
-# def post(self):
-#     data = request.get_json() or request.form
-#     if not data:
-#         raise BadRequest()
-#     units = data['units']
-#     sequnces = request.get('sequences', None)
-#     return ut.compute_variation(units, sequences)
+@app.before_request
+def before_request():
+    g.seq_db = db.rcad_connect()
 
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'seq_db', None)
+    if db is not None:
+        db.close()
+
+
+@mimetype('application/json')
+@app.route('/', methods=['GET'])
+def get_json():
+    return json.dumps(ut.compute_variation(request.args))
+
+
+@mimetype('text/html')
+@app.route('/', methods=['GET'])
+def get_html():
+    if 'units' in request.args:
+        return render_template('results.html',
+                               **ut.compute_variation(request.args))
+    pdbs = []
+    # pdbs = db.list_options(g.seq_db)
+    return render_template('form.html', pdbs=pdbs)
+
+
+@mimetype('application/json')
+@app.route('/', methods=['POST'])
+def post_json():
+    data = request.get_json() or request.form
+    return json.dumps(ut.compute_variations(data))
+
+
+@mimetype('text/html')
+@app.route('/', methods=['POST'])
+def post_html():
+    data = request.get_json() or request.form
+    return render_template('results.html',
+                           **ut.compute_variation(data))
 
 
 if __name__ == '__main__':
