b468bfb9ef49ed857181244b23d6bbedec55be68
Blake Sweeney <blakes.85@gmail.com>
2014-10-15 10:34:17 -0400

    Fix finding logging directory
    This should find the logging directory correctly now. Before we checked for a
    directory with the same name as the file which is silly.

--- prod.py
+++ prod.py
@@ -3,7 +3,8 @@ import logging
 
 from app import app
 
-log_dir = os.path.join(os.path.realpath(__file__), "..", "logs")
+app_dir = os.path.dirname(os.path.realpath(__file__))
+log_dir = os.path.realpath(os.path.join(app_dir, "..", "logs"))
 if os.path.isdir(log_dir):
     handler = logging.FileHandler(os.path.join(log_dir, "app.log"))
     handler.setLevel(logging.INFO)
