caf7ed62987b964af8222d33749db321a438a6f0
Blake Sweeney <blakes.85@gmail.com>
2014-10-15 10:27:21 -0400

    Change location of logging directory
    The logging directory is below the app directory in production so we should
    reflect that change.

--- prod.py
+++ prod.py
@@ -3,7 +3,7 @@ import logging
 
 from app import app
 
-log_dir = os.path.join(os.path.realpath(__file__), "logs")
+log_dir = os.path.join(os.path.realpath(__file__), "..", "logs")
 if os.path.isdir(log_dir):
     handler = logging.FileHandler(os.path.join(log_dir, "app.log"))
     handler.setLevel(logging.INFO)
