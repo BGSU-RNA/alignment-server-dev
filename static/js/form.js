72293abd905bcc84492b88f16ffb9fd790267ba8
Jamie Cannone <jjcannone@bluehen.udel.edu>
2014-11-19 16:09:42 -0600

    Cleared unused function and readability Removed function to unhide hidden blocks -- no longer being used. Added some CR to make brace-matching easier.

--- static/js/form.js
+++ static/js/form.js
@@ -14,7 +14,8 @@ $( window ).load(function() {
     //alert("(i: " + i + ") " + x[i] + " keys " + y[i]); // DEBUG
   }
 
-  $("#pdb-model").change(function() {
+  $("#pdb-model").change(function()
+  {
     var pdbmod = $("#pdb-model").val();
     var arrpdbmod = pdbmod.split("|");
     var pdb = arrpdbmod[0];
@@ -25,35 +26,25 @@ $( window ).load(function() {
 
     // iterate change for all chain selectors
     var chainsel = [ "#pdb-chain", "#chain1", "#chain2", "#chain3", "#chain4", "#chain5" ];
-    for ( var k = 0; k < chainsel.length; k++ ) {
+    for ( var k = 0; k < chainsel.length; k++ )
+    {
       var select = $(chainsel[k]);
       
-      select.disabled = false;
+      //select.disabled = false;
       select.empty().append('{{ null_option() }}}');
 
-      for ( var j = 0; j < y.length; j++ ){
+      for ( var j = 0; j < y.length; j++ )
+      {
         if ( y[j] == "" ) continue; // default case handled above
         
         var arrx = x[j].split("|");
         
-        if ( arrx[0] == pdb && arrx[1] == mod ){
+        if ( arrx[0] == pdb && arrx[1] == mod )
+        {
           select.append('<option value="' + x[j] + '">' + y[j] + '</option>');
         }
       }
     }
-
-    // build new options here
-
-    show_block("jquery_test");
   });
-});
 
-//function show_block(id) {
-//  var e = document.getElementById(id);
-//  if(e != null) { // prevent bugs in IE
-//    if(e.style.display == 'none') {
-//      e.style.display = 'block';
-//    }
-//  }
-//}
-//
+});
