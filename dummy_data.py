68cd62e7c629ae0b01316a834093c5beaba5fcb9
Jamie Cannone <jjcannone@bluehen.udel.edu>
2014-11-19 12:54:29 -0600

    Added LIST_OPTIONS and LIST_STRUCTURES Debugging data for Blake, until we get database connections working for his development environment. May need some tweaking -- doesn't look like I implemented the fixed-width elements completely yet.

--- dummy_data.py
+++ dummy_data.py
@@ -18,3 +18,77 @@ DATA = [
         'LineageName': 'root \ cellular organisms \ Bacteria \ Proteobacteria \ Alphaproteobacteria \ Rhodospirillales \ Acetobacteraceae \ Acetobacter \ Acetobacter subgen. Acetobacter \ Acetobacter aceti'
     }
 ]
+
+LIST_OPTIONS = [
+    {
+        'description': u'Thermus thermophilus SSU rRNA (Bacterial SSU rRNA Seed Alignment)',
+        'chain_id': u'A',
+        'model_number': 1,
+        'requires_translation': True,
+        'pdb': u'1FJG',
+        'option': 1
+    },
+    {
+        'description': u'Escherichia coli SSU rRNA (Bacterial SSU rRNA Seed Alignment)',
+        'chain_id': u'A',
+        'model_number': 1,
+        'requires_translation': False,
+        'pdb': u'2AW7',
+        'option': 1
+    },
+    {
+        'description': u'Escherichia coli FSU rRNA (Bacterial-Chloroplast-Mitochondrial FSU rRNA Alignment)',
+        'chain_id': u'A',
+        'model_number': 1,
+        'requires_translation': False,
+        'pdb': u'2QBG',
+        'option': 1
+    },
+    {
+        'description': u'TEST Escherichia coli LSU rRNA (Bacterial LSU rRNA Private Alignment)',
+        'chain_id': u'B',
+        'model_number': 1,
+        'requires_translation': False,
+        'pdb': u'2QBG',
+        'option': 1
+    },
+    {
+        'description': u'Escherichia coli SSU rRNA (Bacterial SSU rRNA Seed Alignment)',
+        'chain_id': u'A',
+        'model_number': 1,
+        'requires_translation': False,
+        'pdb': u'4KJA',
+        'option': 1
+    }
+]
+
+LIST_STRUCTURES = [
+    {
+        'taxonomy': u'Bacteria ',
+        'organism': u'Thermus thermophilus ',
+        'pdb': u'1FJG',
+        'contents': u'30S (small) ribosomal subunit ',
+        'model_number': 1
+    },
+    {
+        'taxonomy': u'Bacteria ',
+        'organism': u'Escherichia coli ',
+        'pdb': u'2AW7',
+        'contents': u'30S (small) ribosomal subunit ',
+        'model_number': 1
+    },
+    {
+        'taxonomy': u'Bacteria ',
+        'organism': u'Escherichia coli ',
+        'pdb': u'2QBG',
+        'contents': u'50S (large) ribosomal subunit ',
+        'model_number': 1
+    },
+    {
+        'taxonomy': u'Bacteria ',
+        'organism': u'Escherichia coli ',
+        'pdb': u'4KJA',
+        'contents': u'30S (small) ribosomal subunit ',
+        'model_number': 1
+    }
+]
