0d4cf818dee0b42a74049c8681647c0e28899319
Blake Sweeney <blakes.85@gmail.com>
2014-12-05 16:58:58 -0500

    Partially complete form
    The form can now validate and create unit ids. It will display error messages if
    given invalid position input. It is also fairly forgiving about what valid input
    is. We can skip ranges, only enter one endpoint to indicate a single unit and
    such. However, this is not yet complete for several issues:
    
    1. The alerts display will build up the message.
    2. We do not yet build the chain select box.
    3. We don't actually submit anything.
    
    Still this is a good stopping point for the day.

--- static/js/form.js
+++ static/js/form.js
@@ -1,6 +1,86 @@
 $( window ).load(function() {
   'use strict';
 
+  function unitId(unit) {
+    if (!unit.number) {
+      return null;
+    }
+    var parts = [unit.pdb, unit.model, unit.chain, unit.unit, unit.number];
+    if (unit.insertion_code !== '') {
+      parts.push('', '', unit.insertion_code);
+    }
+    return parts.join('|');
+  }
+
+  function unitRange(range) {
+    return range.map(unitId).filter(function(a) { return a; }).join(':');
+  }
+
+  function collection(ranges) {
+    return ranges.map(unitRange).join(',');
+  }
+
+  function showError(element, msg) {
+    var $alert = element.siblings('.alert');
+    $alert
+      .append(msg)
+      .toggle();
+  }
+
+  function validateUnit(unit) {
+    if (!unit.hasOwnProperty('number')) {
+      showError(unit.element, 'Must put valid position');
+      return false;
+    }
+
+    if (unit.number < 0) {
+      showError(unit.element, 'Must put positive number');
+      return false;
+    }
+
+    return true;
+  }
+
+  // Validate range has side effects. It may display error messages on the
+  // page as well as determing if the range is valid.
+  function validateRange(range) {
+    // A range may only have one entry in it. In this case the range is still
+    // valid if the single unit is valid. The single unit may be either start or
+    // stop.
+    var filtered = range.filter(function(unit) { return unit.position; });
+
+    // If we have nothing we can just cal validateUnit as it will complain about
+    // missing data.
+    if (filtered.length === 0) {
+      range.filter(validateUnit);
+      return false;
+    }
+
+    // Otherwise we use the filtered set as the ranges
+    range = filtered;
+
+    var valid = range.filter(validateUnit);
+    if (valid.length !== range.length) {
+      return false;
+    }
+
+    if (range.length === 2) {
+      if (range[0].number > range[1].number) {
+        showError(range[0].element, 'Start must be less than stop');
+        showError(range[1].element, 'Start must be less than stop');
+        return false;
+      } else if (range[0].number === range[1].number) {
+        if (range[0].insertion_code > range[1].insertion_code) {
+          showError(range[0].element, 'Start must be less than stop');
+          showError(range[1].element, 'Start must be less than stop');
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
   function rangeDiv(btn) {
     return $(btn).parents(".range-control-group");
   }
@@ -20,135 +100,74 @@ $( window ).load(function() {
   function clearRange(selector) {
     $(selector).find("input").val("");
     $(selector).find(":selected").removeAttr("selected");
+    $(selector).find(".alert").hide();
   }
 
-  $("#alnsrv").submit(function(event){
-    var units = "";
+  function showFormError(selector, message) {
+    alert(selector, message);
+  }
 
-    if ( $("#pdb-model option:selected").val() == "" ){
-      //  NULL if no selection made, otherwise value
-      alert("Structure not selected!");
-      event.preventDefault();
-    } else {
-      var range = [];
-      for ( var i = 1; i <= 5; i++ ){
-        range[i] = false;
+  $("#submit-ranges").click(function(event){
+    event.preventDefault();
+    $(".alert").hide();
 
-        var c = "#chain" + i;
-        var b = "#beg" + i;
-        var e = "#end" + i;
+    var ranges = [],
+        $pdb = $("#pdb-model option:selected"),
+        pdb = $pdb.data('pdb'),
+        model = $pdb.data('model');
 
-        var cvalue = $(c).val();
-        var bvalue = ( jQuery.trim($(b).val()).length == 0 ) ? 0 : $(b).val();
-        var evalue = ( jQuery.trim($(e).val()).length == 0 ) ? 0 : $(e).val();
-        //  jQuery.trim in two lines directly above to eliminate whitespace
+    var count = parseInt($("#range-control-group").data("range-count"));
+    for(i = 0; i < count; i++) {
+      ranges.push([{pdb: pdb, model: model, unit: '', point: 'start'},
+                  {pdb: pdb, model: model, unit: '', point: 'stop'}]);
+    }
 
-        //  WORKING ABOVE THIS COMMENT
-
-        var bi = 0;
-        var bc = "";
-        var ei = 0;
-        var ec = "";
-
-        //
-        //  If the input value is already an integer, pass it through;
-        //  otherwise, split the string on '|', set the integer value, and
-        //  hold onto the string portion.
-        //
-        //  bi and ei can be used for numeric tests now.
-        //
-
-        if ( +bvalue === parseInt(bvalue) ){
-          bi = bvalue;
-        } else {
-          var barray = bvalue.split('|');
-          bi = parseInt(barray[0]);
-          bc = barray[1];
-        }
-
-        if ( +evalue === parseInt(evalue) ){
-          ei = evalue;
-        } else {
-          var earray = evalue.split('|');
-          ei = parseInt(earray[0]);
-          ec = earray[1];
-        }
-
-        alert("bi: " + bi + "\nei: " + ei ); // DEBUG
-
-        //
-        //  This block needs more work.  Won't properly handle insertion codes
-        //  as written.  The fourth element of the conditional is failing (the
-        //  other four parts work together correctly), not sure why.
-        //
-        //  Blake has code that can test this, but only after submission.  When
-        //  is the best time to test these conditions?
-        //
-        //  Still TO DO:  get this to the point where it will submit the form
-        //  with only the units argument, and not the individual form element
-        //  values (which is what it's doing now, if I weren't either
-        //  suppressing the submit behavior or making syntax errors that allow
-        //  the code to pass that statement).
-        //
-        //  FOR NOW:  only validate that nonzero inputs were assigned.
-        //
-        if (
-                ( 0 < bi )
-            &&  ( 0 < ei )
-            &&  ( cvalue != "" )
-            //&&  ( bi <= ei )          // PROBLEM IS HERE
-            //&&  ( ev <= ( bv + 50 ) ) // PROBLEM IS HERE
-          ) {
-          range[i] = true; // debugging variable
-
-          if ( units ) {
-            units += ",";
-          }
-
-          var bout = ( bc == "" ) ? bi : bi + "|||" + bc;
-          var eout = ( ec == "" ) ? ei : ei + "|||" + ec;
-
-          units += cvalue + "|A|" + bout;
-
-          if ( bout != eout ) {
-            units += ":" + cvalue + "|A|" + eout;
-          }
-          //
-          //  Manual testing shows that strings produced by this string
-          //  builder do produce output without errors.
-          //
-        }
-
-        alert("Range " + i + " validates " + range[i] + "\nChain: " + cvalue + "\nStart: " + bout + "\nStop: " + eout); // DEBUG
-
-        if ( range[i] == false ) {
-          break;
-        }
-      }
-
-      //
-      //  Build units from form elements; submit via window.location.
-      //
-      var http_str = jQuery.param({units: units});
-      //window.location = "?" + http_str;
-
-      //  WORKING BELOW THIS COMMENT
-
-      var alert_str = ( units ) ? "units is set to: " + units : "units not set!";
-
-      alert(alert_str); // DEBUG
-
-      event.preventDefault();
-
-      $("input:submit").attr({
-        //  do this only after validation is complete, so the user can fix any
-        //  errors and still submit the form
-        disabled: "true"
-        , value: "Submitting..."
-        , style: "color:#ab8b8b; text-shadow: 0px 0px 0px; background-color: #dddddd;"
-        // replace with appropriate Bootstrap styles
+    // Pull out all range data
+    function setValue(selector, name, pos) {
+      $(selector).each(function(_, elem) {
+        var $elem = $(elem),
+            index = parseInt($elem.data('range'));
+        pos.forEach(function(p) {
+          ranges[index][p].element = $elem;
+          ranges[index][p][name] = $elem.val();
+        });
       });
     }
+
+    setValue(".chain-selector option:selected", 'chain', [0, 1]);
+    setValue(".start-selector", 'position', [0]);
+    setValue(".stop-selector", 'position', [1]);
+
+    // Parse out the number and insertion code from position
+    ranges = ranges.map(function(range) {
+      return range.map(function(unit) {
+        var number_pattern = /^(\d+)\|?([a-z]*)$/i,
+            matches = number_pattern.exec(unit.position);
+
+        if (matches !== null && matches.length !== 0) {
+          unit.number = parseInt(matches[1]);
+          unit.insertion_code = matches[2];
+        }
+
+        return unit;
+      });
+    });
+
+    // Filter ranges to only those that have entries
+    ranges = ranges.filter(function(range) {
+      if (range[0].element.is(':hidden')) {
+        return false;
+      }
+
+      var filled = range.filter(function(position) {
+        return position.hasOwnProperty('chain') ||
+          position.hasOwnProperty('position');
+      });
+      return filled.length !== 0;
+    });
+
+    // Validate all ranges
+    var submit = ranges.filter(validateRange);
   });
 
   $("#pdb-model").change(function() {
@@ -161,18 +180,11 @@ $( window ).load(function() {
     var arrpdbmod = pdbmod.split("|");
     var pdb = arrpdbmod[0];
     var mod = arrpdbmod[1];
-    //$("input[name=selected_pdb]").val(pdb);
-    //$("input[name=selected_mod]").val(mod);
-    //alert( "pdbmod new value: " + pdbmod + "\npdb: " + pdb + "\nmod: " + mod ); // DEBUG
 
     // iterate change for all chain selectors
-    //var chainsel = [ "#chain1", "#chain2", "#chain3", "#chain4", "#chain5" ];
     for ( var k = 1; k <= 5; k++ )
     {
       var select = $("#chain-select-"+k);
-
-      //select.disabled = false;
-      select.empty().append('{{ null_option() }}}');
 
       for ( var j = 0; j < y.length; j++ )
       {
