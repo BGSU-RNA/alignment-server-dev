f3756713cd6f2b521608b0cc4ad17bf7c4d4033c
Blake Sweeney <blakes.85@gmail.com>
2015-01-30 15:44:09 -0500

    Import app as application and log to stderr
    mod_wsgi requires that the application be called 'application', not 'app'. I've
    made this change. In addition, we can log to stderr to get things into the
    apache logs. This is pretty useful and simplifies the logic of logging a lot. We
    can always make it more complex later.

--- prod.py
+++ prod.py
@@ -1,17 +1,6 @@
-import os
+import sys
 import logging
 
-from app import app
+from app import app as application
 
-app_dir = os.path.dirname(os.path.realpath(__file__))
-log_dir = os.path.realpath(os.path.join(app_dir, "..", "logs"))
-
-if os.path.isdir(log_dir):
-    handler = logging.FileHandler(os.path.join(log_dir, "app.log"))
-    handler.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s:  %(message)s')
-    handler.setFormatter(formatter)
-
-    loggers = [app.logger, logging.getLogger('db')]
-    for logger in loggers:
-        logger.addHandler(handler)
+logging.basicConfig(stream=sys.stdout)
