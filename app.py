b332bcc49cfb4e886c6a92b2e9f1d7e500a8656d
Blake Sweeney <blakes.85@gmail.com>
2015-03-13 15:33:45 -0400

    Rework app to load the options/refactor examples
    We now load the option information from a json file for each request. This
    should speed up generating the main page considerably. This is the first step
    along the pathway of speeding up the server. We can now turn to accessing the
    rcad database happen in the background.
    
    I've also pulled all example code out of app.py and put it into it's own module.
    It is likely that examples and background processing will merge into the same
    code. This is a step toward that. It also clears up app.py.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-import glob
 import hashlib
 
 import simplejson as json
@@ -11,9 +10,10 @@ from flask import render_template
 
 import db
 import utils as ut
-import dummy_data as dd
+import examples
 
 import mimerender
+
 
 app = Flask(__name__)
 mimerender = mimerender.FlaskMimeRender()
@@ -22,51 +22,22 @@ render_json = lambda template, **kwargs: json.dumps(kwargs)
 render_html = lambda template, **kwargs: render_template(template, **kwargs)
 
 
-def load_examples():
-    examples = set()
-    for filename in glob.glob("examples/*.json"):
-        basename = os.path.basename(filename)
-        name = os.path.splitext(basename)[0]
-        examples.add(name)
-    return examples
-
-
-@app.before_request
-def before_request():
-    g.rcad = None
-    if not app.debug:
-        g.rcad = db.rcad_connect(app.config)
-
-
-@app.teardown_request
-def teardown_request(exception):
-    db = getattr(g, 'rcad', None)
-    if db is not None:
-        db.close()
-
-
 @app.errorhandler(db.ProcessingException)
 def handle_problem_range(error):
     return render_template('invalid_range.html'), 400
 
 
-def example_filename(units):
-    name = hashlib.md5(units).hexdigest()
-    path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                        'examples', name + '.json')
-    return path
+def options():
+    with open('conf/options.json', 'rb') as raw:
+        return json.load(raw)
 
 
 def variations(data):
     name = hashlib.md5(data['units']).hexdigest()
     if name in app.config['examples']:
-        filename = example_filename(data['units'])
+        filename = examples.filename(data['units'])
         with open(filename, 'rb') as raw:
-            data = json.load(raw)
-            return data
-
-    if app.debug:
-        return dd.DATA
+            return json.load(raw)
 
     pdb, model, ranges = ut.ranges(data)
     known = db.list_options(g.rcad)
@@ -87,69 +58,12 @@ def variations(data):
     }
 
 
-def configure():
-    #   Needs a fully-tested and viable debugging option... this should be partway there.
-    if app.debug:
-        return dd.LIST_STRUCTURES, dd.LIST_OPTIONS
-    return db.all_options(g.rcad)
-
-
-def group_options():
-    pdbs, opts, tran = configure()
-
-    alignments = {}
-    for alignment in opts:
-        description = alignment['description']
-        pdb = alignment['pdb']
-        if pdb not in alignments:
-            alignments[pdb] = {}
-
-        if description not in alignments[pdb]:
-            alignments[pdb][description] = {
-                'chains': [],
-                'description': description,
-                'alignment_id': description,
-                'diagram': alignment['crw_diagram'],
-                'aln_dir': alignment['crw_aln_dir'],
-                'aln_fil': alignment['crw_aln_fil']
-            }
-        info = alignments[pdb][description]
-        info['chains'].append(alignment['chain_id'])
-
-    data = []
-    for structure in pdbs:
-        entry = {}
-        entry.update(structure)
-        entry['alignments'] = alignments[structure['pdb']].values()
-
-        if 'requires_translation' in entry:
-            del entry['requires_translation']
-        if 'option' in entry:
-            del entry['option']
-
-        data.append(entry)
-
-    return data
-
-
 def result(data):
     result = {'template': 'results.html'}
     result.update(variations(data))
     if app.config.get('log_queries'):
         app.logger.info("Getting variations for: %s", data['units'])
     return result
-
-
-def examples():
-    return [
-        {'description': 'Watson Crick Basepair',
-         'units': '2AW7|1|A||1265,2AW7|1|A||1270'},
-        {'description': 'GNRA Loop', 'units': '2AW7|1|A||1265:2AW7|1|A||1270'},
-        {'description': 'Internal Loop',
-         'units': '2AW7|1|A||580:2AW7|1|A||584,2AW7|1|A||757:2AW7|1|A||761'},
-        {'description': 'Three way junction',
-         'units': '2AW7|1|A||826:2AW7|1|A||829,2AW7|1|A||857:2AW7|1|A||861,2AW7|1|A||868:2AW7|1|A||874'}
-    ]
 
 
 @app.route('/', methods=['GET'])
@@ -164,8 +78,8 @@ def get_html():
 
     return {
         'template': 'form.html',
-        'examples': examples(),
-        'data': group_options()
+        'examples': examples.known(),
+        'data': options()
     }
 
 
@@ -180,7 +94,7 @@ def post_html():
     return result(data)
 
 
-app.config['examples'] = load_examples()
+app.config['examples'] = examples.load()
 config = {}
 if os.path.exists('config.json'):
     with open('config.json', 'rb') as raw:
