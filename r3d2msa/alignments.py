5eb98c78f52fbab7fe3a6564b5c1b4907301ae90
Blake Sweeney <blakes.85@gmail.com>
2015-03-15 18:37:57 -0400

    Add some code for generating alignment downloads
    This can mostly generate strings for alignments. It fails with stockholm format
    right now because we are using the AccssionID as the id of the sequence, but
    there are several different sequences with the same AccessionID in some cases.
    We may just need to use crw ids instead. I will ask Jamie, what the correct
    thing to do here is.

--- r3d2msa/alignments.py
+++ r3d2msa/alignments.py
@@ -1,20 +1,49 @@
 import cStringIO as sio
 
 from Bio import AlignIO
+from Bio.Seq import Seq
+from Bio.Alphabet import generic_rna
+from Bio.SeqRecord import SeqRecord
+from Bio.Align import MultipleSeqAlignment
 
 
-def as_alignment(data):
+class NoAlignmentPossible(Exception):
+    """This is if we cannot generate an alignment out of the data we are given.
+    This can be the case if we are trying to create an alignment for something
+    that has not yet been processed.
+    """
     pass
 
 
+def as_alignment(data):
+    if 'full' not in data:
+        raise NoAlignmentPossible
+
+    sequences = []
+    for entry in data['full']:
+        seq = Seq(entry['CompleteFragment'], generic_rna)
+        record = SeqRecord(seq)
+        record.id = '%s.%s' % (entry['AccessionID'], entry['SeqVersion'])
+        record.description = entry['ScientificName']
+        sequences.append(record)
+    return MultipleSeqAlignment(sequences)
+
+
 def write(name, data):
-    handle = sio.StringIO
+    print(name)
+    handle = sio.StringIO()
     alignment = as_alignment(data)
     AlignIO.write(alignment, handle, name)
-    return handle.get_value()
+    return handle.getvalue()
 
 
-def writer(name):
-    def func(data):
-        write(name, data)
-    return writer
+def fasta(**kwargs):
+    return write('fasta', kwargs)
+
+
+def stockholm(**kwargs):
+    return write('stockholm', kwargs)
+
+
+def clustal(**kwargs):
+    return write('clustal', kwargs)
