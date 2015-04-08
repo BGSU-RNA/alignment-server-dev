9f8f56636aa7357b6beaf0ec93365d376637039e
Blake Sweeney <blakes.85@gmail.com>
2015-04-08 11:41:59 -0400

    Throw a NotAcceptable for bad alignment requests
    If we can't yet generate an alignment then the best thing to do is a throw a
    NotAcceptable I believe. This changes things to do so.

--- r3d2msa/alignments.py
+++ r3d2msa/alignments.py
@@ -10,6 +10,8 @@ from Bio.Alphabet import generic_rna
 from Bio.SeqRecord import SeqRecord
 from Bio.Align import MultipleSeqAlignment
 
+from werkzeug.exceptions import NotAcceptable
+
 
 class NoAlignmentPossible(Exception):
     """This is if we cannot generate an alignment out of the data we are given.
@@ -30,7 +32,7 @@ def as_alignment(data):
     """
 
     if 'full' not in data:
-        raise NoAlignmentPossible
+        raise NotAcceptable("Cannot create alignment until data is processed")
 
     sequences = []
 
