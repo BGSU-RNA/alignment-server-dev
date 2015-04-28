e384a1dd5ce067a53b31b090b5b57026ddcbbd8c
Blake Sweeney <blakes.85@gmail.com>
2015-04-28 11:16:06 -0400

    Reorganize rna3dhub database connection
    It is better to do it only as needed for requests. In addition, I've changed the
    logic a bit to deal with a missing connection string.

--- bin/worker.py
+++ bin/worker.py
@@ -19,14 +19,28 @@ import r3d2msa.background.worker as work
 
 class Worker(work.Worker):
 
+    def rna3dhub(self):
+        try:
+            connection = config.get('rna3dhub', {}).get('connection', None)
+            if not connection:
+                return None
+            engine = create_engine(connection)
+            return hub.db.Hub(engine)
+        except Exception as err:
+            logging.error("Could not connect to rna3dhub")
+            logging.error("Will not have full units for 3D display")
+            logging.exception(err)
+            return None
+
     def expand(self, ranges):
-        if not self.hub:
-            return []
+        rna3dhub = self.rna3dhub()
+        if not rna3dhub:
+            return ''
 
         expanded = []
         for (start, stop) in ranges:
             try:
-                found = self.hub.units(start, stop)
+                found = rna3dhub.units(start, stop)
                 expanded.extend(found)
             except Exception as err:
                 self.logger.error("Failed expanding range %s %s", start, stop)
@@ -88,14 +102,5 @@ if __name__ == '__main__':
     with open(args.config, 'rb') as raw:
         config = json.load(raw)
 
-    rna3dhub = None
-    try:
-        engine = create_engine(config['rna3dhub']['connection'])
-        rna3dhub = hub.db.Hub(engine)
-    except Exception as err:
-        logging.error("Could not connect to rna3dhub")
-        logging.error("Will not have full units for 3D display")
-        logging.exception(err)
-
-    worker = Worker(config, name=args.name, canned=canned, hub=rna3dhub)
+    worker = Worker(config, name=args.name, canned=canned)
     worker()
